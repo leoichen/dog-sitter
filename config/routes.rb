6013cba848974b96e8d81988e37cf1683dc61e4d
Kelvin Chow <c.kelvin.k@gmail.com>
2018-07-30 17:51:06 +0800

    created and added for services-controller, redirect_to might need update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'services/index'
+  get 'services/show'
+  get 'services/new'
+  get 'services/create'
+  get 'services/edit'
+  get 'services/update'
+  get 'services/destroy'
+  get 'services/set_service'
+  get 'services/service_params'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
