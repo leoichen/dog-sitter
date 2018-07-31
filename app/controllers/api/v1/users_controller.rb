ccb70e6485bbd196bb8422f9e48c0a0f43726447
Leo Chen <leoikaichen@gmail.com>
2018-07-31 18:43:39 +0800

    edited some controllers, still need to figure out some of the relationships

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < Api::V1::BaseController
-  before_action :set_user, only: [ :show, :edit, :update, :destroy]
+  before_action :set_user, only: [ :show, :update, :destroy]
+
   def index
     @users = User.all
   end
@@ -7,30 +8,28 @@ class Api::V1::UsersController < Api::V1::BaseController
   def show
   end
 
-  def new
-    @user = User.new
-  end
-
   def create
     @user = User.new(user_params)
-    @user.save
-
-    redirect_to root_path
-  end
-
-  def edit
+    if @user.save
+      render :show
+    else
+      render_error
+    end
   end
 
   def update
-    @user.update(user_params)
-
+   if @user.update(user_params)
     redirect_to root_path
+  else
+    render_ereror
   end
 
   def destroy
-    @user.destroy
-
-    redirect_to root_path
+    if @user.destroy
+      redirect_to root_path
+    else
+      render_error
+    end
   end
 
   private
@@ -40,5 +39,10 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :address, :bio, :gender, :age, :language, :phone_number, :price)
+  end
+
+  def render_error
+    render json: { errors: @restaurant.errors.full_messages },
+    status: :unprocessable_entity
   end
 end
