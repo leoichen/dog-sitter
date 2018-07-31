d76db891b171ea78e9dca9c57f484a54dcbf9ad1
Leo Chen <leoikaichen@gmail.com>
2018-07-31 17:00:44 +0800

    added image url to users and dogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_31_063402) do
+ActiveRecord::Schema.define(version: 2018_07_31_084024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_07_31_063402) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
@@ -69,6 +70,7 @@ ActiveRecord::Schema.define(version: 2018_07_31_063402) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "open_id"
+    t.string "image_url"
   end
 
   add_foreign_key "bookings", "dogs"
