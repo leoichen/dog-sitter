20cba64b674e37ee3b97001e23c93f083671320f
Leo Chen <leoikaichen@gmail.com>
2018-08-02 20:21:33 +0800

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ languages = ["English", "Spanish", "German", "Italian", "French"]
 10.times do
   User.create(first_name: Faker::Name.first_name,
               last_name: Faker::Name.last_name,
-              address: "159 Nanjing West Road,Shanghai",
+              address: "159 Nanjing West Road, Shanghai",
               bio: Faker::SiliconValley.quote,
               gender: ["male", "female"].sample,
               age:(1..80).to_a.sample,
